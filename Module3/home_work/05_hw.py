a44c8445cdf5b38103305d8907c46d036fb767cf
Valeriya07 <91017947+Valeriya07@users.noreply.github.com>
2021-10-02 23:35:20 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -2,5 +2,5 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
-
-# TODO: your code here
+names_new=sorted(names, key=len, reverse=True)
+print(names_new[0])
