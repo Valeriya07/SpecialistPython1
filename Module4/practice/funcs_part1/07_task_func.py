4198a80d8069bcf583871171cfa5fe568621b708
Valeriya07 <91017947+Valeriya07@users.noreply.github.com>
2021-10-10 12:24:30 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,12 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+import datetime
+
+
+def date_time(t):
+    return str(datetime.timedelta(seconds=t))
+
+
+print(date_time(29143))
