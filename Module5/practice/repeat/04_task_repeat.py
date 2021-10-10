fd042d5b4fa024438ea770f9824e93134a30eb1b
Valeriya07 <91017947+Valeriya07@users.noreply.github.com>
2021-10-10 15:27:11 +0300

    Update 04_task_repeat.py

--- Module5/practice/repeat/04_task_repeat.py
+++ Module5/practice/repeat/04_task_repeat.py
@@ -6,5 +6,9 @@
 # веб-странице и вывода списка остальных страниц.
 
 def pagination(num_items, items_on_page):
-    pass
+    if num_items%items_on_page!=0:
+        return num_items // items_on_page+1
+    return num_items // items_on_page
+
+print (pagination(21, 5))
 
